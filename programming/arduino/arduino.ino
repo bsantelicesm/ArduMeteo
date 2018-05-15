2f7405cd5aee3f73003d488b92875a426c87824b
Benjamin Santelices <benjasantelices@hotmail.com>
2018-05-15 15:55:28 -0400

    carpeta arduino y modulo ethernet
    Módulo ethernet parcialmente implementado (no funciona todavía)

--- programming/arduino/arduino.ino
+++ programming/arduino/arduino.ino
@@ -1,3 +1,10 @@
+#include <enc28j60.h>
+#include <EtherCard.h>
+#include <net.h>
+
+static byte mac[] = {0xDD,0xDD,0xDD,0x00,0x01,0x05};
+static byte ip[] = {192,168,103,10}; //Especificar MAC e IP para ENC28J60
+
 #include <SparkFunCCS811.h> //Libreria CCS811
 #include <DHT.h> //Libreria DHT11
 
@@ -32,6 +39,20 @@ void setup() {
     delay(100);
   }
   
+}
+
+static word homePage() {
+  
+ BufferFiller bfill = ether.tcpOffset();
+ bfill.emit_p(PSTR("HTTP/1.0 200 OK\r\n"
+      "Content-Type: text/htmlrnPragma: no-cachernRefresh: 5\r\n\r\n"
+      "<html><head><title>Estación Meteorológica SSCC</title></head>"
+      "<body>"
+      "<p>$D,$D,$D,$D,$D</p>"
+      "</body>"
+      ),Temperatura,Humedad,EnergiaUV,CO2,TVOC,Presion,Precip,VelViento,DirViento); //transmision de los datos a través de HTML
+     
+  return bfill.position();
 }
 
 void loop() {
