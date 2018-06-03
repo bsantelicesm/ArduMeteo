dadc02baa2fdfc26c25e91fd4520c93e1d10828b
Benjamin Santelices <benjasantelices@hotmail.com>
2018-06-03 19:52:38 -0400

    Archivos PCB y bugfix
    -Agregadas visiones de las tarjeras y los Gerber.
    -Bugfixes en el parser.

--- programming/python/html-parser-sql.py
+++ programming/python/html-parser-sql.py
@@ -11,10 +11,19 @@ servidorSQL = sqlite.connect(datos.db) #configuración de SQLite
 
 url = "http://www.fantini.cl" #dirección de estación meteorológica.
 
-while (time())%60 = 0):
-    with urllib.request.urlopen('url') as webDump:
+PrecipInit = 0 #valor inicial para precipitaciones para cuando parta el sketch.
+
+while True:
+
+    while (time()) % 86400 = 0):
+        Precip = 0 #Reiniciar valores de precipitación una vez al día.
+        PrecipInit = PrecipRAW #tomar valor base
+        sleep(100)
+
+    while (time()) % 60 = 0): #repetir una vez por minuto
+        with urllib.request.urlopen('url') as webDump:
             statPage = webDump.read() #urllib busca la pagina y y la guarda como variable
-        parsedText = BeautifulSoup(statPage) #Carga BS para iniciar el parseoself.
+        parsedText = BeautifulSoup(statPage) #Carga BS para iniciar el parseo.
         varsHTML = soup.find("p") # busca el tag <p>
         VarsText = html2text.html2text(varsHTML) #convierte el HTML a string.
 
@@ -23,11 +32,13 @@ while (time())%60 = 0):
         varsList = Temperatura, Humedad, Presion, EnergiaUV, VelViento, DirViento, PrecipRAW, CO2, TVOC
         #convierte una variable de multiples elementos en multiples variables de un elemento.
 
-        #aqui tengo que hacer algo con las precipitaciones.
-
+        Precip = PrecipRAW - PrecipInit #sumar las precipitaciones
+        
         cursorSQL = sqlite.cursor()
 
-        cargarDB = """ INSERT INTO datos (Temperatura, Humedad, Presion, EnergiaUV, VelViento, DirViento, Precip, CO2, TVOC)
-            VALUES (Temperatura, Humedad, Presion, EnergiaUV, VelViento, DirViento, Precip, CO2, TVOC)""" #cargar datos al servidor SQL.
+        cargarDB = """ INSERT INTO datos (Hora, Temperatura, Humedad, Presion, EnergiaUV, VelViento, DirViento, Precip, CO2, TVOC)
+            VALUES (time(), Temperatura, Humedad, Presion, EnergiaUV, VelViento, DirViento, Precip, CO2, TVOC)""" #cargar datos al servidor SQL.
 
         cursorSQL.execute(cargarDB) #Ejecutar comando SQL.
+
+        sleep(100)
