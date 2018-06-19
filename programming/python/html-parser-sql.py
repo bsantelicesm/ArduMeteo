5ae0309a9207cd6221602ac4f035104e9e36697d
Benjamin Santelices <benjasantelices@hotmail.com>
2018-06-19 01:01:55 -0400

    Bugfix Arduino y Python
    -Modificada la salida del arduino para hacerla más amigable para procesar.
    -Bugfixes y ordenados varios en Python.

--- programming/python/html-parser-sql.py
+++ programming/python/html-parser-sql.py
@@ -1,44 +1,37 @@
-import urllib
+import urllib2
 from bs4 import BeautifulSoup
-import html2text
-import sqlite3
 import time
+import MySQLdb
 
-print "Estación Meteorológica SS.CC Manquehue"
-print "Copyright 2018 Benjamin Santelices"
+print("Estación Meteorológica SS.CC Manquehue")
+print("Copyright 2018 Benjamin Santelices")
 
-servidorSQL = sqlite.connect(datos.db) #configuración de SQLite
-
-url = "http://www.fantini.cl" #dirección de estación meteorológica.
+url = "file:///C:/Users/benja/Documents/GitHub/sscctiempo/programming/html/arduino-sample-output.htm" #dirección de estación meteorológica.
 
 PrecipInit = 0 #valor inicial para precipitaciones para cuando parta el sketch.
 
+SQL = MySQLdb.connect(host="localhost", user="root", passwd="manquehue", db="meteorologia") #conección al servidor mysql local.
+
 while True:
 
-    while (time()) % 86400 = 0):
+    while (time.time()) % 86400 == 0:
         Precip = 0 #Reiniciar valores de precipitación una vez al día.
         PrecipInit = PrecipRAW #tomar valor base
         sleep(100)
 
-    while (time()) % 60 = 0): #repetir una vez por minuto
-        with urllib.request.urlopen('url') as webDump:
-            statPage = webDump.read() #urllib busca la pagina y y la guarda como variable
-        parsedText = BeautifulSoup(statPage) #Carga BS para iniciar el parseo.
-        varsHTML = soup.find("p") # busca el tag <p>
-        VarsText = html2text.html2text(varsHTML) #convierte el HTML a string.
-
-        varsList = VarsText.split(",") #separa los valores usando las comas como referencia.
-        print varsList
-        varsList = Temperatura, Humedad, Presion, EnergiaUV, VelViento, DirViento, PrecipRAW, CO2, TVOC
-        #convierte una variable de multiples elementos en multiples variables de un elemento.
+    while (time.time()) % 60 == 0: #repetir una vez por minuto
+        parsedText = BeautifulSoup(urllib2.urlopen(url).read(), "html.parser") #Carga BS para iniciar el parseo.
+        varsHTML = parsedText.find("p").get_text().strip().split(",") # busca el tag <p> y convierte el HTML a string.
+         #separa los valores usando las comas como referencia.
+        varsFloat = [float(i) for i in varsHTML] #convierte el texto a punto flotante.
+        Temperatura, Humedad, Presion, EnergiaUV, VelViento, DirViento, PrecipRAW, CO2, TVOC = varsFloat #separa variables.
+        print (varsFloat)
 
         Precip = PrecipRAW - PrecipInit #sumar las precipitaciones
-        
-        cursorSQL = sqlite.cursor()
 
-        cargarDB = """ INSERT INTO datos (Hora, Temperatura, Humedad, Presion, EnergiaUV, VelViento, DirViento, Precip, CO2, TVOC)
-            VALUES (time(), Temperatura, Humedad, Presion, EnergiaUV, VelViento, DirViento, Precip, CO2, TVOC)""" #cargar datos al servidor SQL.
-
-        cursorSQL.execute(cargarDB) #Ejecutar comando SQL.
+        cargarDB = """INSERT INTO datos (Hora, Temperatura, Humedad, Presion, EnergiaUV, VelViento, DirViento, Precip, CO2, TVOC)
+            VALUES (time.time(), temperatura, humedad, presion, energiauv, velviento, dirviento, precip, co2, tvoc)""" #cargar datos al servidor SQL.
+        SQL.cursor().execute(cargarDB)
+        SQL.commit()
 
         sleep(100)
