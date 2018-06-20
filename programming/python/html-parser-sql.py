89f54ac8a199c99a1da70efe50d5ac33f4261ee9
bsantelicesm <benjasantelices@hotmail.com>
2018-06-20 00:06:08 -0400

    Completada Implementación Python-SQL

--- programming/python/html-parser-sql.py
+++ programming/python/html-parser-sql.py
@@ -24,29 +24,27 @@ while True:
         Precip = 0 #Reiniciar valores de precipitación una vez al día.
         PrecipInit = PrecipRAW #tomar valor base
         print("Valores de precipitación reiniciados.")
-        sleep(100)
+        time.sleep(1)
 
     while (time.time()) % 60 == 0: #repetir una vez por minuto
+        curTime = time.time()
+
         parsedText = BeautifulSoup(urllib2.urlopen(url).read(), "html.parser") #Carga BS para iniciar el parseo.
         varsHTML = parsedText.find("p").get_text().strip().split(",") # busca el tag <p> y convierte el HTML a string.
          #separa los valores usando las comas como referencia.
         varsFloat = [float(i) for i in varsHTML] #convierte el texto a punto flotante.
         Temperatura, Humedad, Presion, EnergiaUV, VelViento, DirViento, PrecipRAW, CO2, TVOC = varsFloat #separa variables.
 
-        curTime = time.time()
         print (curTime)
         print (varsFloat) #toma la hora e imprime los valores y el timestamp.
 
         Precip = PrecipRAW - PrecipInit #sumar las precipitaciones
 
-        SQL.cursor().execute("""INSERT INTO datos (timestamp, temperatura, humedad, presion, energiauv, velviento, dirviento, precip, co2, tvoc)
-<<<<<<< HEAD
-        VALUES ('%f','%f','%f','%f','%f','%f','%f','%f','%f','%f');""",(float(curTime), float(Temperatura), float(Humedad), float(Presion), float(EnergiaUV), float(VelViento), float(DirViento), float(Precip), float(CO2), float(TVOC)))
-=======
-        VALUES ('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s');""",(float(curTime), float(Temperatura), float(Humedad), float(Presion), float(EnergiaUV), float(VelViento), float(DirViento), float(Precip), float(CO2), float(TVOC)))
->>>>>>> 6490938d8c7757c6554eadd91415da173b17ab1a
+        SQL.cursor().execute("""INSERT INTO datos (measuringtime, temperatura, humedad, presion, energiauv, velviento, dirviento, precip, co2, tvoc)
+        VALUES (NOW(),'%s','%s','%s','%s','%s','%s','%s','%s','%s');""",(float(Temperatura), float(Humedad), float(Presion), float(EnergiaUV), float(VelViento), float(DirViento), float(Precip), float(CO2), float(TVOC)))
         SQL.commit() #cargar los valores a la base de datos.
         print ("Valores cargados correctamente!")
         print (" ")
 
-        sleep(100)
+        time.sleep(1)
+
