fb0af599418db75c608816d5e384e6420391c12e
Benjamin Santelices <benjasantelices@hotmail.com>
2018-11-18 21:46:48 -0300

    Agregar PHP y cleanup

---
+++ programming/php/sql.php
@@ -0,0 +1,17 @@
+<?php
+  $server = "localhost";
+  $username = "username";
+  $password = "password"; // parámetros para SQL
+
+  $
+
+  $conn = new mysqli($server, $username, $password);
+
+  if ($conn->connect_error) {
+    die("ERR" . $conn->connect_error)
+  }
+
+  function pedirDatos($dato) {
+
+  }
+?>
